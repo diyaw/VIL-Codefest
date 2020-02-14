90a64fe2dfc75910049e1db714ed5b835159d8d5
Diya Wadhwani <diyawadhwani62@gmail.com>
2020-02-14 14:11:45 +0530

    AR filter added

--- OTPLogin/app/src/main/java/com/example/otplogin/TresFragment.java
+++ OTPLogin/app/src/main/java/com/example/otplogin/TresFragment.java
@@ -1,11 +1,13 @@
 package com.example.otplogin;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -21,8 +23,22 @@ public class TresFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+
+        View RootView = inflater.inflate(R.layout.fragment_three, container, false);
+        Button button1 = (Button)RootView.findViewById(R.id.arButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),  AugmentedFacesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return RootView;
     }
+
+
+
+
 
 }
