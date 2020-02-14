dca6a5877806184f50bceedfca65c61a5fbbd18c
Diya Wadhwani <diyawadhwani62@gmail.com>
2020-02-14 10:32:57 +0530

    splash updated

--- BottomNavigationBar/app/src/main/java/com/example/bottomnamviagtionbar/CautroFragment.java
+++ BottomNavigationBar/app/src/main/java/com/example/bottomnamviagtionbar/CautroFragment.java
@@ -8,10 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CautroFragment extends Fragment {
+
+    private CircleMenu circleMenu;
 
 
     public CautroFragment() {
@@ -22,8 +35,19 @@ public class CautroFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_four, container, false);
+
+        View RootView = inflater.inflate(R.layout.fragment_four, container, false);
+
+
+
+        return RootView;
     }
 
-}
+
+    }
+
+
+
+
+
+
